aa01be84f3bc1526c29bfb32d7b8264cd468fd7b
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2023-10-27 14:37:36 +0300

    first commit

--- main.sh
+++ main.sh
@@ -3,9 +3,12 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+wget http://deb.debian.org/debian/pool/non-free/i/intel-media-driver-non-free/intel-media-driver-non-free_23.3.5+ds1.orig.tar.xz
+tar -xf ./intel-media-driver-non-free_23.3.5+ds1.orig.tar.xz
+mv -v ./media-driver-intel-media-23.3.5 ./media-driver-intel-media
+cp -rvf ./debian ./media-driver-intel-media/
+cd ./media-driver-intel-media/
+for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
 # Get build deps
 apt-get build-dep ./ -y
