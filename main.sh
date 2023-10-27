a2f2d2549ab8d24ba6b08fc13cab9276459f5609
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-27 15:19:33 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,7 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-wget http://deb.debian.org/debian/pool/non-free/i/intel-media-driver-non-free/intel-media-driver-non-free_23.3.5+ds1.orig.tar.xz
-tar -xf ./intel-media-driver-non-free_23.3.5+ds1.orig.tar.xz
-mv -v ./media-driver-intel-media-23.3.5 ./media-driver-intel-media
+git clone ./media-driver-intel-media https://github.com/intel/media-driver.git -b intel-media-23.3.4
 cp -rvf ./debian ./media-driver-intel-media/
 cd ./media-driver-intel-media/
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
