8ce974225f5e4621dd73d521db95b9a3a861dd55
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-27 15:19:46 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-git clone ./media-driver-intel-media https://github.com/intel/media-driver.git -b intel-media-23.3.4
+git clone ./media-driver-intel-media https://github.com/intel/media-driver.git -b intel-media-23.4.0
 cp -rvf ./debian ./media-driver-intel-media/
 cd ./media-driver-intel-media/
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
